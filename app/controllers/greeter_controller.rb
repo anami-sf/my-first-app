ca84dd60aa45f0d5f118927f22df1b750f8cbedd
Ana <anami823@gmail.com>
2018-11-29 11:10:28 -0800

    Added formatted time and date methods to greeter helper. Also added @name and @time instance variables to controller. Finally, added html text in hello view page to display name and current date and time.

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,5 +1,10 @@
 class GreeterController < ApplicationController
   def hello
+    randome_names = %w( Nathan Ella Sophie )
+
+    @name = randome_names.sample
+    @time = Time.now
+    # @day = Time.wday Time.yday
   end
 
   def goodbye
--- app/helpers/greeter_helper.rb
+++ app/helpers/greeter_helper.rb
@@ -1,2 +1,10 @@
 module GreeterHelper
+    def formatted_time(time)
+        time.strftime("%l:%M%p")
+    end
+
+    def formatted_date(time)
+        time.strftime("%A %B %e, %Y")
+    end
+        
 end
