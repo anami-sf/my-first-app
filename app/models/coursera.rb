5271e66326ae8b45c648466ce7803e044e2c1537
Ana <anami823@gmail.com>
2018-11-29 14:32:24 -0800

    Added @search_term and @courses in Courses controller. Also added table of courses in the index view page.

--- app/models/coursera.rb
+++ app/models/coursera.rb
@@ -2,15 +2,13 @@ class Coursera
     include HTTParty
 
     base_uri 'https://api.coursera.org/api/catalog.v1/courses'
-    courses_params fields:"smallIcon,shortDescription", q:"search"
+    default_params fields:"smallIcon,shortDescription", q:"search"
     format :json
 
     def self.for(term)
         # The get method will return a hash 
         # and 'elements' is a key which is also a hash iniside a hash.
-        get("", query: { query: term})[elements]
+        get("", query: { query: term})["elements"]
     end
-
-
 
 end
