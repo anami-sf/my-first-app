a396534aa975dfdcf550722fcc1603b46f422d8a
Ana <anami823@gmail.com>
2018-11-29 07:33:23 -0800

    Updated routes in routes.rb file to bypass /greeter. Also set root to greeter#hello

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'greeter/hello'
-  get 'greeter/goodbye'
+  
+  root 'greeter#hello'
+  
+  get 'hello', to: 'greeter#hello'
+  get 'goodbye', to: 'greeter#goodbye'
+
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
